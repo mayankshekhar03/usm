1da825d72a1b613b6942fcbde00d7828adccf718
Glitch (usm) <none>
2018-01-14 16:59:05 +0000

    🌔🏢 Checkpoint server.js:415617/206 package.json:415617/7

--- server.js
+++ server.js
@@ -2,8 +2,27 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var path    = require('path');
 const bodyParser = require('body-parser');
+var mongodb = require('mongodb').MongoClient;
+
+//database connection
+var database;
+
+new mongodb('mongodb://ds251807.mlab.com:51807/usm', {
+    auth: {
+       user: 'mayankshekhar03',
+       password: 'sharda#1',
+    }
+}).connect(
+    (err, db) => {
+      if (err) return console.error(err);
+      console.log('Database connected');
+      database = db.db('foo'); 
+});
+//~database connection 
 
 var app     = express();
+
+//making staticfiles available
 app.use(express.static('public'));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'})); 
